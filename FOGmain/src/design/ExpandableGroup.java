8a30d94343963de635c89c5a053da0dd8784b6b7
Mikael Vanggaard Jensen <Mikael.vang@gmail.com>
2013-05-09 20:31:36 +0200

    Manglede lige noget til kevin :D

---
+++ FOGmain/src/design/ExpandableGroup.java
@@ -0,0 +1,25 @@
+package design;
+
+import java.util.ArrayList;
+
+import dk.vinael.domain.Party;
+
+public class ExpandableGroup {
+
+	private String nameOfGroup;
+	private ArrayList<Party> parties = new ArrayList<Party>();
+	
+	public ArrayList<Party> getParties() {
+		return parties;
+	}
+	public void setParties(ArrayList<Party> parties) {
+		this.parties = parties;
+	}
+	public String getNameOfGroup() {
+		return nameOfGroup;
+	}
+	public void setNameOfGroup(String nameOfGroup) {
+		this.nameOfGroup = nameOfGroup;
+	}
+
+}
